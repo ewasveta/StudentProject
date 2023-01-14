1d676f7473aa892ca918a84b4e2752f04f548ec5
EwaSveta <ewasveta@gmail.com>
2023-01-14 17:23:17 +0200

    full site. Master Page.

---
+++ MasterPage/footer.js
@@ -0,0 +1,43 @@
+document.write('\
+<div>\
+<h4>ABOUT ME</h4>\
+<p>\
+Cascading Style Sheets (CSS) is used to format<br>\
+the layout of a webpage.<br><br>\
+With CSS, you can control the color, font, <br>\
+the size of text, the spacing between elements, <br>\
+how elements are positioned and laid out, <br>\
+what background images or background colors <br>\
+are to be used, different displays for different<br>\
+devices and screen sizes, and much more!<br>\
+</p>\
+</div>\
+<div>\
+<h4>MY PROGECTS</h4>\
+<section>\
+    <img src="./Images/Small/1.jpg">\
+    <img src="./Images/Small/2.jpg">\
+    <img src="./Images/Small/3.jpg">\
+    <img src="./Images/Small/4.jpg">\
+    <img src="./Images/Small/5.jpg">\
+    <img src="./Images/Small/6.jpg"></section></div>\
+<div>\
+<h4>NEWSLETTER REGISTRATION</h4>\
+<p>\
+Subscribe today to receive all the latest Day news via<br>\
+email. You may unsubscribe from this service at any <br>\
+time.</p>\
+<summary>\
+    <button>ENTER EMAIL</button>\
+    <button>SUSCRIBE</button>\
+</summary></div>\
+\
+'); 
+
+
+
+// document.write('\
+// \
+//     <h1>This is my include file</h1>\
+// \
+// ');
---
+++ MasterPage/header.js
@@ -0,0 +1,19 @@
+document.write('\
+\
+<h5>STUDENT&reg LTD.</h5>\
+<div>\
+    <ul>\
+        <li><a href="./home.html">HOME</a></li>\
+        <li><a href="./about_us.html">ABOUT US</a></li>\
+        <li><a href="./gallery.html">GALLERY</a></li>\
+        <li><a href="./contact.html">CONTACT</a></li>\
+    </ul>\
+</div>\
+\
+');
+
+// document.write('\
+// \
+//     <h1>This is my include file</h1>\
+// \
+// ');
